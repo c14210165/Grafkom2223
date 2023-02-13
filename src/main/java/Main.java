034e8dd09441766ed0f3868fd1adb6e89e35f382
KOMP_SI\INFOR <alden>
2023-02-13 10:17:32 +0700

    alden

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import Engine.Window;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL30.*;
+
+public class Main {
+    private Window window = new Window(800, 800, "Hello Cok");
+
+    public void init() {
+        window.init();
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while (window.isOpen()) {
+            window.update();
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            GL.createCapabilities();
+
+            glDisableVertexAttribArray(0);
+
+            glfwPollEvents();
+        }
+    }
+
+    public void run() {
+        init();
+        loop();
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
